382b5533f2b74aa0d07d687bd99f695b1e7b21cf
SPYRIDON KATSAMPOS <spkatsampos@gmail.com>
2021-06-09 16:10:02 +0300

    Update Product.java

--- demo/src/main/java/com/example/demo/product/Product.java
+++ demo/src/main/java/com/example/demo/product/Product.java
@@ -1,9 +1,17 @@
 package com.example.demo.product;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import javax.persistence.*;
 
 @Entity
 @Table
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Product {
     @Id
     @SequenceGenerator(
@@ -13,60 +21,18 @@ public class Product {
     )
     @GeneratedValue(
            strategy = GenerationType.SEQUENCE,
-            generator ="producr_sequence"
+            generator ="product_sequence"
     )
     private long id;
     private String name;
     private String ingredients;
     private String characteristics;
 
-public Product(){}
-
-    public Product(long id, String name, String ingredients, String characteristics) {
-        this.id = id;
-        this.name = name;
-        this.ingredients = ingredients;
-        this.characteristics = characteristics;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getIngredients() {
-        return ingredients;
-    }
-
-    public void setIngredients(String ingredients) {
-        this.ingredients = ingredients;
-    }
-
-    public String getCharacteristics() {
-        return characteristics;
-    }
-
-    public void setCharacteristics(String characteristics) {
-        this.characteristics = characteristics;
-    }
-
     public Product(String name, String ingredients, String characteristics) {
         this.name = name;
         this.ingredients = ingredients;
         this.characteristics = characteristics;
     }
-
     @Override
     public String toString() {
         return "Product{" +
@@ -75,10 +41,6 @@ public Product(){}
                 ", ingredients='" + ingredients + '\'' +
                 ", characteristics='" + characteristics + '\'' +
                 '}';
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
 }
